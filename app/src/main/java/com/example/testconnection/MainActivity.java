3dcc333e45e5e7240858a5eba458c6b7b7c64ccc
ryantakeshihagerty <ryanhagerty29@gmail.com>
2020-03-28 02:14:31 -0700

    successfully read data from remote database

--- app/src/main/java/com/example/testconnection/MainActivity.java
+++ app/src/main/java/com/example/testconnection/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.testconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        downloadJSON("http://ineedtophp.000webhostapp.com/simpletest.php");
+    }
+
+    private void downloadJSON(final String webURL) {
+        class DownloadJSON extends AsyncTask<Void, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(webURL);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = br.readLine()) != null) {
+                        sb.append(json + "\n"); // add to JSON string
+                    }
+                    return sb.toString().trim();    //return json string
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+
+        DownloadJSON getJSON = new DownloadJSON();
+        getJSON.execute();
+    }
+
+    private void loadIntoListView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        String[] output = new String[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            output[i] = obj.getString("name") + " " + obj.getString("price");   // NOTE: relates to column names
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, output);
+        listView.setAdapter(arrayAdapter);
     }
 }
